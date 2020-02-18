493dea17d36bc1bfe6d0c4ce5b86abe9b24a4609
gilanglie <gilzlie@gmail.com>
2020-02-18 20:59:09 +0700

    1st commit

---
+++ build/precache-manifest.a09a34b80ddf9dd0459eda6dc902a4c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ebd5e88331d6709c8537d4abb155c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daf1eaca3b2655d9d145",
+    "url": "/static/js/2.58e30803.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.58e30803.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1928fe53dfd5098e61d",
+    "url": "/static/js/main.267467da.chunk.js"
+  },
+  {
+    "revision": "8e0db4b4d4b22fb737e2",
+    "url": "/static/js/runtime-main.53db2103.js"
+  }
+]);
